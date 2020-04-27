a2dd7af79e07e0ca7f497d8a73406af920fc8161
phuocnd69 <dinhphuoc762@gmail.com>
2020-04-28 00:19:37 +0700

    2 files modified

--- src/app/add-dish/add-dish.component.ts
+++ src/app/add-dish/add-dish.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../Dish';
 import { DishService } from '../dish.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-dish',
@@ -9,6 +10,16 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./add-dish.component.scss']
 })
 export class AddDishComponent implements OnInit {
+    form = new FormGroup({
+      id: new FormControl('',Validators.required),
+      name: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      img: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required),
+
+    })
+
+
     dish: Dish = new Dish();
   constructor(
     private dishService: DishService,
@@ -22,5 +33,19 @@ addDish(){
   this.dishService.addDish(this.dish).subscribe(data => 
     this.router.navigateByUrl('/admin/dish-manager'));
     alert("Thêm thành công");
+}
+
+clear(){
+  var id = <HTMLInputElement>document.getElementById("id");
+  id.value = null;
+  var name = <HTMLInputElement>document.getElementById("name");
+  name.value = null;
+  var price = <HTMLInputElement>document.getElementById("price");
+  price.value = null;
+  var img = <HTMLInputElement>document.getElementById("img");
+  img.value = null;
+  var desc = <HTMLInputElement>document.getElementById("desc");
+  desc.value = null;
+  
 }
 }
